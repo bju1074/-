0fb79c06d38240810510d34a2b51c90cc4cf224e
bju1074 <42632288+bju1074@users.noreply.github.com>
2018-09-06 16:27:35 +0900

    Add files via upload

---
+++ 자바기초실습/src/코드업풀이/Main1004.java
@@ -0,0 +1,9 @@
+package 코드업풀이;
+
+public class Main1004 {
+
+	public static void main(String[] args) {
+			System.out.println("\'Hello'");
+	}
+
+}
---
+++ 자바기초실습/src/코드업풀이/Main1020.java
@@ -0,0 +1,16 @@
+package 코드업풀이;
+
+import java.util.Scanner;
+
+public class Main1020 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		s.useDelimiter("\r|\n|-");
+		String a = s.next();
+		String b = s.next();
+		System.out.println(a + b);
+
+	}
+
+}
