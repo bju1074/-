d748a06bb011289ce38ef42b5a8ed66669c42198
bju1074 <42632288+bju1074@users.noreply.github.com>
2018-09-27 16:26:38 +0900

    Add files via upload

--- 자바과제/src/종합실습과제/문제24.java
+++ 자바과제/src/종합실습과제/문제24.java
@@ -3,7 +3,12 @@ package 종합실습과제;
 public class 문제24 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("[ASCII 코드표]");
+		for(int i=32;i<=127;i++) {
+			System.out.printf("%3d %c  ", i, (char)i);
+			if((i%10)==1)
+				System.out.println();
+		}
 
 	}
 
