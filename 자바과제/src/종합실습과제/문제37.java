87beb1b08f680c902ee36c242bd6bca83e0c4d36
bju1074 <42632288+bju1074@users.noreply.github.com>
2018-09-27 15:59:00 +0900

    Add files via upload

---
+++ 자바과제/src/종합실습과제/문제37.java
@@ -0,0 +1,26 @@
+package 종합실습과제;
+
+import java.util.Scanner;
+
+public class 문제37 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("자연수: ");
+		int a = sc.nextInt();
+		System.out.print("우박수열: 22");
+		while(a>1) {
+			if(a%2 == 0) {
+				a = a/2;
+				System.out.print(" " + a);
+			}
+			else if(a%2 == 1) {
+				a = a*3 +1;
+				System.out.print(" " + a);
+			}
+		}
+		sc.close();
+
+	}
+
+}
