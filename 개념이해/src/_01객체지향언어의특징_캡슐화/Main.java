e46439aa576f6f0afc3fdde58f8ce27cd4096a41
bju1074 <42632288+bju1074@users.noreply.github.com>
2018-10-18 15:11:39 +0900

    Add files via upload

--- 개념이해/src/_01객체지향언어의특징_캡슐화/Main.java
+++ 개념이해/src/_01객체지향언어의특징_캡슐화/Main.java
@@ -1,9 +1,17 @@
 package _01°´Ã¼ÁöÇâ¾ð¾îÀÇÆ¯Â¡_Ä¸½¶È­;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame jf=new JFrame(); 
+		jf.setTitle("Åë½Å°ú");
+		jf.setSize(300,300);
+		jf.setLocationRelativeTo(null);
+		jf.setVisible(true);
 
 	}
 
---
+++ 개념이해/src/_03객체지향언어의특징_다형성/Test1.java
@@ -0,0 +1,25 @@
+package _03°´Ã¼ÁöÇâ¾ð¾îÀÇÆ¯Â¡_´ÙÇü¼º;
+
+public class Test1 {
+	void print(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	void print(String a, int b) {
+		System.out.println(a + b);
+	}
+
+	void print(int a, String b) {
+		System.out.println(a + b);
+	}
+
+	void print(String a, String b) {
+		System.out.println(a + b);
+	}
+
+	public void bark() {
+		System.out.println("¸Û¸Û");
+		
+	}
+
+}
