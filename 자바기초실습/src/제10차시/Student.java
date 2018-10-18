35df110c684749e9449cc704248db4ced1eeb81c
bju1074 <42632288+bju1074@users.noreply.github.com>
2018-10-18 16:29:40 +0900

    Add files via upload

---
+++ 자바기초실습/src/제10차시/Student.java
@@ -0,0 +1,14 @@
+package Á¦10Â÷½Ã;
+
+public class Student {
+
+	public static void main(String[] args) {
+		public String name;
+		int grade;
+		int ban;
+		int number;
+		private String telephone ;
+
+	}
+
+}
